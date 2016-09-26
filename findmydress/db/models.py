9a4b03ac59c1d43a1097752eb6940da653385eee
David Hughes <davehughes@uber.com>
2016-09-26 08:18:57 -0700

    Updated image import script to put files in S3 instead of on local disk

--- findmydress/db/models.py
+++ findmydress/db/models.py
@@ -1,11 +1,13 @@
 import json
+import os
 
+import boto3
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('postgresql+psycopg2://localhost/modscrape', echo=True)
+engine = create_engine('postgresql+psycopg2://localhost/findmydress', echo=True)
 Session = sessionmaker()
 Session.configure(bind=engine)
 
@@ -35,7 +37,6 @@ class ItemImage(Base):
     original_url = Column(String)
     checksum = Column(String)
     position = Column(Integer)
-    image_path = Column(String)
     image_s3_url = Column(String, nullable=True)
 
     item_id = Column(Integer, ForeignKey('items.id'))
@@ -53,35 +54,61 @@ class ImageDerivative(Base):
     id = Column(Integer, primary_key=True)
     type = Column(String)
     original_image = Column(Integer, ForeignKey('item_images.id'))
-    image_path = Column(String)
     image_s3_url = Column(String)
 
 
-def import_scraped_items(output_json_path):
+def import_scraped_items(output_json_path, file_path_root):
     session = Session()
 
     items_json = json.load(open(output_json_path))
     for item_json in items_json:
-        item = Item(
-            label=item_json['item_title'],
-            detail_url=item_json['detail_url'],
-            )
-        session.add(item)
-        session.commit()
+        item = (session.query(Item)
+               .filter(Item.detail_url==item_json['detail_url'])
+               .one_or_none())
+
+        if not item:
+            item = Item(
+                label=item_json['item_title'],
+                detail_url=item_json['detail_url'],
+                )
+            session.add(item)
+            session.commit()
         
         item_images = []
         for img, item_img in zip(item_json['images'], item_json['item_images']):
             if item_img['type'] != 'high-res':
                 continue
 
+            image = (session.query(ItemImage)
+                    .filter(ItemImage.checksum==img['checksum'])
+                    .one_or_none())
+            if image:
+                continue
+
+            image_path = os.path.join(file_path_root, img['path'])
+            s3_path = os.path.join('files/images/full', os.path.basename(image_path))
+            image_s3_url = upload_image_file(image_path, s3_path)
+
             image = ItemImage(
                 original_url=item_img['url'],
                 position=item_img['position'],
                 checksum=img['checksum'],
-                image_path=img['path'],
+                image_s3_url=image_s3_url,
                 item_id=item.id,
                 )
             item_images.append(image)
 
         session.add_all(item_images)
         session.commit()
+
+
+def upload_image_file(path, s3_path):
+    if not os.path.isfile(path):
+        raise ValueError("No file at path {}".format(path))
+
+    bucket_name = 'findmydress'
+    s3 = boto3.resource('s3')
+    key = s3.Object(bucket_name, s3_path)
+    key.put(Body=open(path, 'rb'))
+
+    return 's3://{}'.format(os.path.join(bucket_name, s3_path))
